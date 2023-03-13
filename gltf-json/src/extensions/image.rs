75af6d43cd0a04baf9fd23308f15b4031baa1004
Sean Rennie <rnnsea001@gmail.com>
2023-03-13 08:48:15 +0000

    remove packages already supplied by gltf crate

--- gltf-json/src/extensions/image.rs
+++
@@ -1,6 +0,0 @@
-use gltf_derive::Validate;
-use serde_derive::{Deserialize, Serialize};
-
-/// Image data used to create a texture.
-#[derive(Clone, Debug, Default, Deserialize, Serialize, Validate)]
-pub struct Image {}
--- gltf-json/src/extensions/skin.rs
+++
@@ -1,6 +0,0 @@
-use gltf_derive::Validate;
-use serde_derive::{Deserialize, Serialize};
-
-/// Joints and matrices defining a skin.
-#[derive(Clone, Debug, Default, Deserialize, Serialize, Validate)]
-pub struct Skin {}
