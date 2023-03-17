ceca99e0993962d0248f597f8a1c8ae8952eeb77
Sean Rennie <rnnsea001@gmail.com>
2023-03-17 12:39:47 +0000

    Update export.rs

--- src/export.rs
+++ src/export.rs
@@ -1,10 +1,86 @@
 use std::borrow::Cow;
+use std::io::{ErrorKind, Write};
+use std::{fs, path};
 
 use crate::binary;
 use crate::buffer;
+use crate::buffer::Source;
 use buffer::{align_to_multiple_of_four, to_padded_byte_vector, Data};
 
 use crate::{Document, Error};
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+/// The type of output to write.
+pub enum Output {
+    /// Output standard glTF.
+    /// Can only be used if the imported glTF has buffer uri's pre-set.
+    /// Todo: allow mutation of buffer uri's
+    Standard,
+    /// Output binary glTF.
+    Binary,
+}
+
+/// Export a glTF document
+/// TODO: support images
+pub fn export(
+    output: Output,
+    path: &str,
+    document: Document,
+    buffers: Vec<Data>,
+    // _image_buffers: Vec<image::Data>,
+) -> Result<(), Error> {
+    match output {
+        Output::Standard => export_to_gltf(path, document, buffers),
+        Output::Binary => export_to_glb(path, document, buffers),
+    }
+}
+
+/// Export a glTF document
+pub fn export_to_gltf(
+    path: &str,
+    document: Document,
+    buffers: Vec<Data>,
+    // _image_buffers: Vec<image::Data>,
+) -> Result<(), Error> {
+    // write each buffer to a file
+
+    let gltf_path = path::Path::new(&path).with_extension("gltf");
+    let directory = gltf_path.parent();
+    if let Some(dir) = directory {
+        fs::create_dir_all(dir).expect("I/O error");
+    }
+
+    // write binary files
+    for buffer in document.buffers() {
+        // get the source of the buffer; if it's not a file, then we can't write it;
+        match buffer.source() {
+            Source::Uri(uri) => {
+                let uri_path = path::Path::new(uri);
+                let mut writer = if let Some(dir) = directory {
+                    fs::File::create(dir.join(uri_path)).expect("I/O error")
+                } else {
+                    fs::File::create(uri_path).expect("I/O error")
+                };
+
+                let data = buffers[buffer.index()].clone();
+                let bin = to_padded_byte_vector(data.0);
+                writer.write_all(&bin).expect("I/O error");
+                Ok(())
+            }
+            Source::Bin => Err(Error::Io(std::io::Error::new(
+                ErrorKind::InvalidData,
+                "Buffer has no URI set",
+            ))),
+        }
+        .expect("Validation error");
+    }
+
+    let writer = fs::File::create(gltf_path).expect("I/O error");
+    let root = document.into_json();
+    json::serialize::to_writer_pretty(writer, &root).expect("Serialization error");
+
+    Ok(())
+}
 
 /// Todo: need docs
 pub fn export_to_glb(
@@ -13,6 +89,12 @@ pub fn export_to_glb(
     buffers: Vec<Data>,
     // _image_buffers: Vec<image::Data>,
 ) -> Result<(), Error> {
+    let glb_path = path::Path::new(&path).with_extension("glb");
+    let directory = glb_path.parent();
+    if let Some(dir) = directory {
+        fs::create_dir_all(dir).expect("I/O error");
+    }
+
     // create a single blob of data from each buffer and image
     let mut blob = Vec::<u8>::new();
     for buffer in document.buffers() {
@@ -42,7 +124,7 @@ pub fn export_to_glb(
         json: Cow::Owned(json_string.into_bytes()),
     };
 
-    let writer = std::fs::File::create(path).expect("I/O error");
+    let writer = std::fs::File::create(glb_path).expect("I/O error");
     glb.to_writer(writer).expect("glTF binary output error");
     Ok(())
 }
